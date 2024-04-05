15c902f932778c1bd9978ffa5b183b1f542418bd
dqf <dqfzj@foxmail.com>
2024-04-05 11:11:53 +0800

    小配置更新

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # coding=utf-8
+# ------------------------------------------------------------------
+# File Name:        main.py
+# Author:           dqfzj@foxmail.com
+# Created:          2024/4/5 9:53
+# Description:
+# ------------------------------------------------------------------
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/wechat')
 def index():
     return {
         "msg": "success",
@@ -15,5 +21,4 @@ def index():
 
 
 if __name__ == '__main__':
-    # app.run(host='112.10.236.251', port=9000, debug=True)
-    app.run(host='192.168.16.167', port=9000, debug=True)
+    app.run(host='127.0.0.1', port=9000)
--- webService/Handle.py
+++ webService/Handle.py
@@ -1,5 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 # coding=utf-8
+# ------------------------------------------------------------------
+# File Name:        Handle.py
+# Author:           dqfzj@foxmail.com
+# Created:          2024/4/5 9:53
+# Description:
+# ------------------------------------------------------------------
 
 class Handle(object):
     def GET(self):
