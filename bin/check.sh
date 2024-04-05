122fb968c3a79406d3d20e1cec3fe1c5583ac336
dqf <dqfzj@foxmail.com>
2024-04-05 14:43:25 +0800

    增加定时监控任务

--- bin/check.sh
+++ bin/check.sh
@@ -1,6 +1,8 @@
+#!/usr/bin/env bash
 path=/var/log/gpt_wechat/
 
 process=`ps aux | grep main.py | grep -v grep`;
-if [ "$process" == "" ];then
-        python3 /opt/wechat/gongzhonghao/gpt_wechat/main.py
+if [[ "$process" == "" ]]
+then
+    python3 /opt/wechat/gongzhonghao/gpt_wechat/main.py
 fi
